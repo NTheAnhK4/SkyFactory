53c03331eab4a0caa7b99cbff4e001b49a7dfcb4
Nguyen The Anh <nguyentheanh14102004@gmail.com>
2026-05-24 23:42:21 +0700

    Nuget, Odin, Dotween

---
+++ Assets/_Game/TestScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Button]
+    private void Test()
+    {
+        
+    }
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
