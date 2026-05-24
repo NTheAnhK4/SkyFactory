c8bbc29ec4dbb358fb5fd6b43e71305abf7cd8f0
Nguyen The Anh <nguyentheanh14102004@gmail.com>
2026-05-25 00:00:01 +0700

    virtual router

--- Assets/_Game/TestScript.cs
+++ Assets/_Game/TestScript.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using R3;
+using VitalRouter;
 
 public class TestScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    private ICommandPublisher _publisher;
     [Button]
     private void Test()
     {
